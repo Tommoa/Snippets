3905e053b9e99ac7b277cd68932a78bd9d1388af
Tom Almeida <=>
2017-06-22 16:25:46 +0800

    Actually made it so that point2d would work

--- geometry/point2d.cpp
+++ geometry/point2d.cpp
@@ -1,42 +1,44 @@
-#include <bits/stdc++.h>
+#include <cmath>
+#include <utility>
+#include <vector>
 
 using namespace std;
 
-template<typename T> class Point2D {
+template<typename T> struct Point2D {
 public:
     T x, y;
     Point2D(T x, T y, T z) {
-        this.x = x;
-        this.y = y;
+        this->x = x;
+        this->y = y;
     }
     Point2D(Point2D<T> &old) {
-        this.x = old.x;
-        this.y = old.y;
+        this->x = old.x;
+        this->y = old.y;
     }
     Point2D<T> operator+(const Point2D<T> other) {
-        Point<T> tmp(*this);
+        Point2D<T> tmp(*this);
         tmp.x += other.x;
         tmp.y += other.y;
         return tmp;
     }
     Point2D<T> operator-(const Point2D<T> other) {
-        Point<T> tmp(*this);
+        Point2D<T> tmp(*this);
         tmp.x -= other.x;
         tmp.y -= other.y;
         return tmp;
     }
     T dot(const Point2D<T> other) {
-        return this.x*other.x + this.y*other.y;
+        return this->x*other.x + this->y*other.y;
     }
     T cross(const Point2D<T> other) {
-        return this.x*(other.y) - other.x*this.y;
+        return this->x*(other.y) - other.x*this->y;
     }
     double magnitude() {
-        sqrt(pow(this.x, 2) + pow(this.y, 2));
+        sqrt(pow(this->x, 2) + pow(this->y, 2));
     }
 };
 
-template<typename T> pair<T, bool> poly_winding(const vector<Point2D<T>>& poly, Point2D<T> pt) {
+template<typename T> pair<T, bool> poly_winding(const vector<Point2D<T> >& poly, Point2D<T> pt) {
 	int wn = 0;
 	Point2D<T> prev = poly.back() - pt;
 	for (Point2D<T> curr : poly) {
@@ -49,7 +51,7 @@ template<typename T> pair<T, bool> poly_winding(const vector<Point2D<T>>& poly, 
 	return pair<T, bool>(wn, false);
 }
 
-template<typename T> T poly_twice_area(const vector<Point2D<T>>& poly) {
+template<typename T> T poly_twice_area(const vector< Point2D<T> >& poly) {
 	coord_t twice_area = 0;
 	Point2D<T> prev = poly.back();
 	for (Point2D<T> curr : poly) {
@@ -58,11 +60,11 @@ template<typename T> T poly_twice_area(const vector<Point2D<T>>& poly) {
 	}
 	return twice_area;
 }
-template<typename T> T poly_area(const vector<Point2D<T>>& poly) { return poly_twice_area(poly) / 2;}
+template<typename T> T poly_area(const vector< Point2D<T> >& poly) { return poly_twice_area(poly) / 2;}
 
-template<typename T> vector<Point2D<T>> convex_hull(vector<Point2D<T>> pts) {
+template<typename T> vector< Point2D<T> > convex_hull(vector< Point2D<T> > pts) {
 	sort(pts.begin(), pts.end());
-	vector<Point2D<T>> upr, lwr;
+	vector< Point2D<T> > upr, lwr;
 	for (Point2D<T> &pt : pts) {
 		while (upr.size() >= 2 && (upr.back() - upr[upr.size()-2]).cross(pt - upr.back()) >= 0)
 			upr.pop_back();
