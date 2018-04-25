530c120f2e1866d87a41b73c0315c849842b90dd
Tom Almeida <tommoa256@gmail.com>
2018-04-25 18:07:43 +0800

    Updated template_poly.cpp for blog post

--- others/template_poly.cpp
+++ others/template_poly.cpp
@@ -1,22 +1,39 @@
 #include <iostream>
+#include <vector>
+#include <functional>
 
-template <typename T, typename R> auto map(T array[], long numel, R (*f)(T)) {
-	auto result = new R[numel];
-	for (long i = 0; i < numel; i++) {
-		result[i] = f(array[i]);
-	}
+#define poly_map(v, f) map(v, f<decltype(v)::value_type>)
 
-	return result;
+template <typename T, typename F, typename R = typename std::result_of<F&&(T&&)>::type> auto map(std::vector<T> arr, F&& f) -> decltype(std::vector<R>()) {
+    std::vector<R> out(arr.size());
+    for (long i = 0; i < arr.size(); ++i) {
+            out[i] = f(arr[i]);
+    }
+    return out;
 }
 
-template <typename T> auto poly_incr(T x) { return x + 1; }
+template <typename T> auto poly_incr(T x) -> decltype(x+1) { return x + 1; }
+int long_incr(long x) { return ++x; }
+
+template <typename T> void print_array(std::vector<T> arr) {
+    std::cout << "{ ";
+    for (auto t : arr)
+        std::cout << t << " ";
+    std::cout << "}" << std::endl;
+}
+
+struct Incr {
+    long incr(long x) { return ++x; };
+};
 
 int main() {
-	long numel = 5;
-	long array[] = {1, 2, 3, 4, 5};
+    std::vector<long> array = {1, 2, 3, 4, 5};
+    Incr i;
+    print_array(array);
 
-	auto res = map(array, numel, poly_incr<decltype(poly_incr(array[0]))>);
-	for (int a = 0; a < numel; a++) {
-		std::cout << "res[" << a << "]: " << res[a] << std::endl;
-	}
+    print_array(map(array, std::bind(&Function::incr, &i, std::placeholders::_1)));
+
+    print_array(poly_map(array, poly_incr));
+
+    print_array(map(array, long_incr));
 }
